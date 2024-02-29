e5323ab39d9a91bbc064c8074257bb70c7398aec
yurill <rawityurill@gmail.com>
2024-02-29 23:54:55 +0700

    routes hospitals to route hotel

--- routes/hospitals.js
+++ routes/hospitals.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {getHospitals, getHospital, createHospital, updateHospital, deleteHospital} = require('../controllers/hospitals');
+const {getHotels, getHotel, createHotel, updateHotel, deleteHotel} = require('../controllers/hotels');
 
 const appointmentRouter = require('./appointments');
 
@@ -8,8 +8,8 @@ const router = express.Router();
 const {protect, authorize} = require('../middleware/auth');
 
 // Re-route into other resource routers
-router.use('/:hospitalId/appointments', appointmentRouter);
+router.use('/:hotelId/appointments', appointmentRouter);
 
-router.route('/').get(getHospitals).post(protect, authorize('admin'), createHospital);
-router.route('/:id').get(getHospital).put(protect, authorize('admin'), updateHospital).delete(protect, authorize('admin'), deleteHospital);
+router.route('/').get(getHotels).post(protect, authorize('admin'), createHotel);
+router.route('/:id').get(getHotel).put(protect, authorize('admin'), updateHotel).delete(protect, authorize('admin'), deleteHotel);
 module.exports = router;

f7dded46b60fbbf2af696189da1f5d0a9bd8b7fd
Yurills <rawityurill@gmail.com>
2024-03-26 22:48:31 +0700

    edit HOST

--- server.js
+++ server.js
@@ -25,7 +25,9 @@ app.use('/api/v1/otp', otp);
 
 
 const PORT=process.env.PORT || 5000;
-const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ',  PORT));
+const server = app.listen(PORT, console.log('Server running in ', 
+process.env.NODE_ENV,
+ 'on ' + process.env.HOST + ":" + PORT));
 
 process.on('unhandledRejection', (err, promise) =>{
     console.log(`Error: ${err.message}`);
